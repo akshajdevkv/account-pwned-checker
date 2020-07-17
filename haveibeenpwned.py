55e9e2fb4ff0a7e2de1fde5700a626e76b9dd9b7
Akshaj Dev <62797898+akshajdevkv@users.noreply.github.com>
2020-07-17 13:07:02 +0530

    Initial Commit

---
+++ haveibeenpwned.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Firefox(executable_path= './geckodriver')
+
+try:
+	#type your email address here
+	emails = ['yourname@email.com','myname@email.com','thiername@email.com']
+	for email in emails:
+		driver.get('https://haveibeenpwned.com/')
+		email_input = driver.find_element_by_id('Account')
+		email_input.send_keys(email)
+		driver.find_element_by_id('searchPwnage').click()
+		time.sleep(3)
+		result_negative = driver.find_element_by_xpath('/html/body/div[4]/div[1]/div/div/div[1]/h2').text 
+		result_positive = driver.find_element_by_xpath('/html/body/div[3]/div/div/div[1]/h2').text
+		breached_website = driver.find_elements_by_class_name('pwnedCompanyTitle')
+		compromised_data = driver.find_elements_by_class_name('dataClasses')
+		print(email)
+		print(result_negative+result_positive)
+		for i,j in zip(breached_website,compromised_data):
+			print('breached website:'+i.text)
+			print(j.text)
+		print('')
+		print('')
+
+except Exception as e:
+	print(e)
+	#closes the browser
+	driver.quit()
+
